d6bd45c6f41a854d2d1ff984fee4211721b78471
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-04-30 00:18:52 +0100

    Added most of the tests to Options - same error occuring for all of them

--- client/src/components/Options/Options.test.js
+++ client/src/components/Options/Options.test.js
@@ -4,19 +4,60 @@ import userEvent from '@testing-library/user-event';
 
 
 describe('Options',() => {
-  beforeEach(() => {
+    let optionsStub1 = {A:'True', B:'False'};
+    let optionsStub2 = {A:'Test1', B:'Test2', C:'Test3', D:'Test4' };
+    let disabledStub = false;
+    let setDisabledStub;
     
-    renderWithReduxProvider(<Options/>)
-  })
 
+
+  
   test('it renders a submit button', () => {
+    renderWithReduxProvider(<Options options={optionsStub1} disabled={disabledStub} setDisabled={setDisabledStub}/>)
     let btn = screen.getByRole('button',{name: 'submit'});
     expect(btn).toBeInTheDocument();
   })
 
+  test("it renders a 'False' option button", () => {
+    renderWithReduxProvider(<Options options={optionsStub1} disabled={disabledStub} setDisabled={setDisabledStub}/>)
+    let btn = screen.getByRole('button',{name: 'A.False'});
+    expect(btn).toBeInTheDocument();
+  })
+
+  test("it renders a 'True' option button", () => {
+    renderWithReduxProvider(<Options options={optionsStub1} disabled={disabledStub} setDisabled={setDisabledStub}/>)
+    let btn = screen.getByRole('button',{name: 'B.True'});
+    expect(btn).toBeInTheDocument();
+  })
+
+  test("it renders multiple choice option 1", () => {
+    renderWithReduxProvider(<Options options={optionsStub2} disabled={disabledStub} setDisabled={setDisabledStub}/>)
+    let btn = screen.getByRole('button',{name: 'A.Test1'});
+    expect(btn).toBeInTheDocument();
+  })
+
+  test("it renders multiple choice option 2", () => {
+    renderWithReduxProvider(<Options options={optionsStub2} disabled={disabledStub} setDisabled={setDisabledStub}/>)
+    let btn = screen.getByRole('button',{name: 'B.Test2'});
+    expect(btn).toBeInTheDocument();
+  })
+
+  test("it renders multiple choice option 3", () => {
+    renderWithReduxProvider(<Options options={optionsStub2} disabled={disabledStub} setDisabled={setDisabledStub}/>)
+    let btn = screen.getByRole('button',{name: 'C.Test3'});
+    expect(btn).toBeInTheDocument();
+  })
+
+  test("it renders multiple choice option 4", () => {
+    renderWithReduxProvider(<Options options={optionsStub2} disabled={disabledStub} setDisabled={setDisabledStub}/>)
+    let btn = screen.getByRole('button',{name: 'B.Test4'});
+    expect(btn).toBeInTheDocument();
+  })
+
+  //Add handleSubmit test
+  //Add handleSelect test
+
 })
-//<button handleSelect={stubHandleSelect}></button>
-//
 
   
   
